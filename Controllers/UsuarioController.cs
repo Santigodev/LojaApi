d0a5ecc3ad91467fc1ca5298ba1fbb851e06242b
Santigodev <shilton.santiago@gmail.com>
2024-11-01 20:07:22 -0300

    Carrinho de Compras + Registro dos Repositories

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using LojaApi.Models;
+using LojaApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaApi.Controllers
+{
+    [Route("api/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UsuarioRepository _usuarioRepository;
+
+        public UsuarioController(UsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        [HttpPost("cadastrar-usuario")]
+        public async Task<IActionResult> CadastrarUsuario([FromBody] Usuario usuario)
+        {
+            var usuarioId = await _usuarioRepository.CadastrarUsuarioDB(usuario);
+
+            return Ok(new { mensagem = "Usuário cadastrado com sucesso!", usuarioId });
+        }
+
+        [HttpGet("listar-usuarios")]
+        public async Task<IActionResult> ListarUsuarios()
+        {
+            var usuarios = await _usuarioRepository.ListarUsuariosDB();
+
+
+            return Ok(usuarios);
+        }
+    }
+}
