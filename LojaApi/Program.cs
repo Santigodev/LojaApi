c68b65b7f3fa4a346ce242762a1ecd0925b10187
Santigodev <shilton.santiago@gmail.com>
2024-11-01 19:03:47 -0300

    Registrando o Service do Repository + Swagger + Dados de acesso ao DB

--- LojaApi/Program.cs
+++ LojaApi/Program.cs
@@ -1,10 +1,37 @@
+using LojaApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+//Conecta no banco
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//Registra o Service do Repository
+builder.Services.AddSingleton<ProdutoRepository>(provider => new ProdutoRepository(connectionString));
+
+
+//Swagger Parte 1
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+});
+
 var app = builder.Build();
+
+//Swagger Parte 2
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Loja V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
 
 // Configure the HTTP request pipeline.
 
